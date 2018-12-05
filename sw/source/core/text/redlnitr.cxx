7d1e6c383f6780b81f756152f72fbb5f2cbbe68c
Michael Stahl <Michael.Stahl@cib.de>
2018-12-05 16:26:22 +0100

    sw_redlinehide_4b: fix wrong handling of IsDelLastPara() redlines
    CheckParaRedlineMerge(): Fully deleted SwTextNode followed by a table,
    as seen in ooo81405-1.odt.
    
    Change-Id: I2a7d935a9cedcb66aeaa9dce418b1fab758f1784

--- sw/source/core/text/redlnitr.cxx
+++ sw/source/core/text/redlnitr.cxx
@@ -124,10 +124,24 @@ CheckParaRedlineMerge(SwTextFrame & rFrame, SwTextNode & rTextNode,
                 }
                 pTmp->SetRedlineMergeFlag(SwNode::Merge::Hidden);
             }
-            pNode = pEnd->nNode.GetNode().GetTextNode();
-            assert(pNode);
-            nodes.push_back(pNode);
-            pNode->SetRedlineMergeFlag(SwNode::Merge::NonFirst);
+            // note: in DelLastPara() case, the end node is not actually merged
+            // and is likely a SwTableNode!
+            if (!pEnd->nNode.GetNode().IsTextNode())
+            {
+                assert(pEnd->nNode != pStart->nNode);
+                assert(nodes.back() == pNode);
+                assert(nodes.back() == pNode->GetNodes()[pEnd->nNode.GetIndex() - 1]);
+                if (nodes.back() != &rTextNode)
+                {   // something might depend on last merged one being NonFirst?
+                    pNode->SetRedlineMergeFlag(SwNode::Merge::NonFirst);
+                }
+            }
+            else
+            {
+                pNode = pEnd->nNode.GetNode().GetTextNode();
+                nodes.push_back(pNode);
+                pNode->SetRedlineMergeFlag(SwNode::Merge::NonFirst);
+            }
         }
         nLastEnd = pEnd->nContent.GetIndex();
     }
